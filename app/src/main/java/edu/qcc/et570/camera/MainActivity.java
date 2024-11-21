8ab1adbb52cb3a8bc847d3113de6b426403734bf
dn51 <dnandahp@gmail.com>
2024-11-21 17:52:04 -0500

    Initial commit

---
+++ app/src/main/java/edu/qcc/et570/camera/MainActivity.java
@@ -0,0 +1,35 @@
+package edu.qcc.et570.camera;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.button1);
+        btn.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i=new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                startActivity(i);
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
